4368ff7f486239cc3b9f7a25b448556e1f4dfc75
alegresor <agsorokin3@gmail.com>
2026-04-25 11:13:41 -0500

    householder skeletons

--- agsutil/__init__.py
+++ agsutil/__init__.py
@@ -4,13 +4,15 @@ from .algos import (
     lm_opt,
     minres,
     minres_qlp_cs,
-    to_unitary,
-    from_unitary,
     )
 from .utils import (
     print_data_signatures,
     Timer, 
     logcomb,
+    to_unitary_expskewh,
+    from_unitary_expskewh,
+    to_unitary_householder,
+    from_unitary_householder,
     )
 from .plots import (
     mpl_setup,
